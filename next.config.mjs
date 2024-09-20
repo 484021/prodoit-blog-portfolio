701792b38d930dd86790dc149a5981786605807f
484021 <bhoopalsanthosh@gmail.com>
2024-09-20 10:12:26 -0400

    feat: next.config edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
+const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
