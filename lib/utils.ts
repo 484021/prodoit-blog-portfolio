74afa30d4664bbf8baf9ba45587876df48a53976
484021 <bhoopalsanthosh@gmail.com>
2024-09-22 14:13:02 -0400

    feat: contact form working

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,29 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const fetchUrl = "http://localhost:3000/api";
+export const validateString = (value: unknown, maxLength: number) => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
+  return true;
+};
+
+export const getErrorMessage = (error: unknown): string => {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
+
+  return message;
+};
