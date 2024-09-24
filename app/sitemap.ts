4876152e33bb7616f26293c3d50a9888e82fedab
484021 <bhoopalsanthosh@gmail.com>
2024-09-23 21:56:30 -0400

    feat: site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,18 @@
-export const baseUrl = "https://prodoit.dev/"
+import { POSTS } from "@/lib/constants";
+import { getBlogPosts } from "./blog/utils";
+
+export const baseUrl = "https://next-blog-cj.vercel.app";
+
+export default async function sitemap() {
+  const blogs = getBlogPosts().map((post) => ({
+    url: `${baseUrl}/blog/${post.metadata.category}/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }));
+
+  const routes = POSTS.map((route) => ({
+    url: `${baseUrl}${route.href}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
+  return [...blogs, ...routes];
+}
