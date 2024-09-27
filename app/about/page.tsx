94b0cd2954119843db00f46dbaa065b672427e19
484021 <bhoopalsanthosh@gmail.com>
2024-09-26 21:10:14 -0400

    feat: 2 new blog posts

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return (
-    <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
-      About
-    </main>
-  );
-}
